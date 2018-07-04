f7b708b4dccff6c0753750ec4b6a859bf1ac85c6
soruly <soruly@gmail.com>
2018-07-04 23:08:36 +0800

    Update burn-ass script

--- burn-ass.sh
+++ burn-ass.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-if [[ -d $1 ]]; then
-  echo "Dir"
-elif [[ -f $1 ]] ; then
-  if [[ $# -eq 2 ]] ; then
-    if [[ ${2: -4} == ".ass" ]] ; then
-        echo "Burn ass on video"
-        #"$0" "$(pwd)/$1" "$(pwd)/$2"
-      fi
-    else
-      echo "\$2 is not an ass file"
-    fi
-  else
-    echo "Simple video convert"
-  fi
-else
-  echo "\$1 is not a file or directory"
-fi
+#if [[ -d $1 ]]; then
+#  echo "Dir"
+#elif [[ -f $1 ]] ; then
+#  if [[ $# -eq 2 ]] ; then
+#    if [[ ${2: -4} == ".ass" ]] ; then
+#        echo "Burn ass on video"
+#        #"$0" "$(pwd)/$1" "$(pwd)/$2"
+#      
+#    else
+#      echo "\$2 is not an ass file"
+#    fi
+#  else
+#    echo "Simple video convert"
+#  fi
+#else
+#  echo "\$1 is not a file or directory"
+#fi
 
 export FONTCONFIG_FILE="$(pwd)/font/font.conf"
 export FONTCONFIG_PATH="$(pwd)/font"
@@ -36,12 +36,14 @@ echo "Created fontconfig at $FONTCONFIG_FILE"
 #  <edit name="family" mode="assign"><string>1_GBK</string></edit>
 #</match>
 
+mkdir -p "$(pwd)/output/"
+
 ffmpeg -y \
 -ss 00:00:00 \
 -i "$1" \
 -map_metadata -1 -map_chapters -1 \
--c:v libx264 -r 24000/1001 -pix_fmt yuv420p -profile:v high -preset ultrafast \
--vf scale=-1:-1,ass='$2' \
+-c:v libx264 -r 24000/1001 -pix_fmt yuv420p -profile:v high -preset medium \
+-vf scale=-1:-1,ass="'$2'" \
 -c:a aac \
 -ac 2 \
 -map 0:v -map 0:a:language:jpn \
